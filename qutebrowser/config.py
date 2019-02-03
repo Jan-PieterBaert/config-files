331387149cc359437926409dc3646d83796f7f20
Jan-Pieter Baert <janpieter.baert@ugent.be>
2019-02-03 02:52:20 +0100

    qute updates
    Signed-off-by: Jan-Pieter Baert <janpieter.baert@ugent.be>

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -44,7 +44,7 @@ c.content.autoplay = False
 # User agent to send. Unset to send the default. Note that the value
 # read from JavaScript is always the global value.
 # Type: String
-c.content.headers.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+c.content.headers.user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'
 
 # List of URLs of lists which contain hosts to block.  The file can be
 # in one of the following formats:  - An `/etc/hosts`-like file - One
