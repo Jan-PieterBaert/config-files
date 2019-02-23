4ce52ed743b41db72fd128f41850c6e9a5a1cb8a
Jan-Pieter Baert <janpieter.baert@ugent.be>
2019-02-23 16:44:13 +0100

    added scrolling for brightness

--- i3/i3scripts/backlight.sh
+++ i3/i3scripts/backlight.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "$(xbacklight -get)%"
+current_brightness=$(xbacklight -get | grep -o "^.*\." | grep -o "[0-9]*")
+
+case $BLOCK_BUTTON in
+  4) /fast_files/git_repos/custum_scripts/changebacklight.sh up ;;
+  5) /fast_files/git_repos/custum_scripts/changebacklight.sh down ;;
+esac
+
+echo "$current_brightness%"
+
 # cat /sys/class/backlight/intel_backlight/actual_brightness | sed 's/^/100*/' | sed 's/$/\/937/' | bc | sed 's/$/%/'
